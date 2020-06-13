d4f1a4e16e144f24c5debb1b21c0c9258c212109
Zi Wang <zwang2@linkedin.com>
2020-06-13 11:11:38 -0700

    update image upload base url

--- src/components/utils/restUtils.js
+++ src/components/utils/restUtils.js
@@ -1,3 +1,7 @@
+
+const BASE_URL = process.env.NODE_ENV === 'development' ?
+                  'http://localhost:8080'
+                  : 'https://smart-classroom-server.wl.r.appspot.com';
 /**
  * Upload a recording snapshot image
  * @param {string} dataUrl the image dataUrl to send
@@ -6,8 +10,7 @@ export function uploadSnapshot(dataUrl) {
   const body = new FormData();
   body.append('imageData', dataUrl);
 
-  //TODO configure base url
-  fetch('http://localhost:8000/image/upload', {
+  fetch(`${BASE_URL}/image/upload`, {
     method: 'POST',
     body,
   });
