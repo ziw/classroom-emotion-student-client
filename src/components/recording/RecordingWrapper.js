4cafd3ed9b9d589384c2a5739e254eeae8adaca1
Zi Wang <wangzi0112@gmail.com>
2020-06-24 14:51:40 -0700

    add face-api.js to draw face detection border in canvas

--- src/components/recording/RecordingWrapper.js
+++ src/components/recording/RecordingWrapper.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Button } from 'antd';
 import './RecordingWrapper.css';
-import { uploadSnapshot } from '../../utils';
+import { uploadSnapshot, BASE_URL } from '../../utils';
 import Authorize from '../Authorize';
+import * as faceapi from 'face-api.js';
+
+const minConfidence = 0.5;
+const options = new faceapi.SsdMobilenetv1Options({ minConfidence })
 
 export default class RecordingWrapper extends React.Component {
 
@@ -10,7 +14,9 @@ export default class RecordingWrapper extends React.Component {
     super(props);
     this.videoPlayer = React.createRef();
     this.stagingCanvas = React.createRef();
+    this.faceApiCanvas = React.createRef();
     this.intervalId = undefined;
+    this.faceApiInterval = undefined;
     this.state = {
       cameraSupported: 'mediaDevices' in navigator,
       recording: false,
@@ -18,11 +24,17 @@ export default class RecordingWrapper extends React.Component {
     }
   }
 
+  async componentDidMount() {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(`${BASE_URL}/`);
+    window.videoEl = this.videoPlayer.current;
+    window.canvasEl = this.faceApiCanvas.current;
+  }
+
   resizeCanvas() {
     this.setState({
       canvasDimension: {
-        w: this.videoPlayer.current.videoWidth,
-        h: this.videoPlayer.current.videoHeight,
+        w: this.videoPlayer.current.scrollWidth,
+        h: this.videoPlayer.current.scrollHeight,
       }
     });
   }
@@ -47,6 +59,7 @@ export default class RecordingWrapper extends React.Component {
       if(this.intervalId) {
         clearInterval(this.intervalId);
       }
+      clearTimeout(this.faceApiInterval);
       videoPlayer.srcObject.getVideoTracks().forEach(track => track.stop());
       videoPlayer.srcObject = undefined;
       this.setState({ recording: false });
@@ -60,6 +73,20 @@ export default class RecordingWrapper extends React.Component {
     }
   }
 
+  async detectFace() {
+    const videoEl = this.videoPlayer.current;
+    const result = await faceapi.detectSingleFace(videoEl, options)
+
+    if (result) {
+      const canvas = this.faceApiCanvas.current;
+      const dims = faceapi.matchDimensions(canvas, videoEl, true);
+      if(dims && dims.width && dims.height){
+        faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
+      }
+    }
+    this.faceApiInterval = setTimeout(() => this.detectFace());
+  }
+
   render() {
     if(!this.state.cameraSupported) {
       return <div>Your device does not support camera, or you did not give access to use camera</div>
@@ -71,11 +98,18 @@ export default class RecordingWrapper extends React.Component {
         <Button onClick={this.toggleRecording.bind(this)}>
           {this.state.recording ? 'Stop Video' : 'Start Video'}
         </Button>
-        <video autoPlay ref={ this.videoPlayer } width="100%"></video>
+        <div style={{ position: 'relative', margin: 'auto', width: '640px' }}>
+          <video autoPlay ref={ this.videoPlayer } onLoadedMetadata={this.detectFace.bind(this)} width="640px" height="480px"></video>
+          <canvas id="overlay" ref={ this.faceApiCanvas }
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            width={ 640 }
+            height={ 480 }
+          />
+        </div>
         <canvas ref={ this.stagingCanvas }
           className="recording-wrapper__canvas"
-          width={ this.state.canvasDimension.w }
-          height={ this.state.canvasDimension.h }
+          width={ 640 }
+          height={ 480 }
         />
       </div>
     );
